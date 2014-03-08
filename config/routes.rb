6165a31dedbab0b3ab65191812a96d5aed7bdf85
Michael Pierce <michael@affektive.com>
2014-03-07 18:34:20 -0700

    Fixed conflict

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,8 @@ Weowe::Application.routes.draw do
   resources :weowe_forms
   resources :users
 
-<<<<<<< HEAD
-  root :to => "static_pages#dashboard"
-
-  get 'users/help'
-=======
-  devise_for :users
   root 'static_pages#dashboard'
-
   get 'static_pages/help'
->>>>>>> 4e11a304e07d8f1821360321d38036c36f3c8fd8
   get 'static_pages/contact'
   get 'static_pages/about'
   match '/dashboard', to: 'static_pages#dashboard', via: 'get'
