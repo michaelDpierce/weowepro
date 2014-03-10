8b506d335e52dbe23847ea2cf02df75d6bb24443
Michael Pierce <michael@affektive.com>
2014-03-10 12:00:58 -0600

    Small fixes

--- app/helpers/weowe_forms_helper.rb
+++ app/helpers/weowe_forms_helper.rb
@@ -1,9 +1,8 @@
 module WeoweFormsHelper
 
   def verify_total(parts_value, labor_value)
-    return '' unless !parts_value && !labor_value
-    parts = parts_value.nil? ? '' : parts_value
-    labor = labor_value.nil? ? '' : labor_value
+    parts = parts_value.nil? ? 0 : parts_value
+    labor = labor_value.nil? ? 0 : labor_value
     total = parts + labor
   end
 
