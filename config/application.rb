63c8d7012c0bbaebaa1c1358cf83ef5968708215
Michael Pierce <michael@affektive.com>
2014-03-16 10:37:27 -0600

    More rubocop

--- config/application.rb
+++ config/application.rb
@@ -9,28 +9,36 @@ require 'sprockets/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+SESSIONS = proc { |controller| user_signed_in? ? 'application' : 'devise' }
+REGISTRATION = proc { |controller| user_signed_in? ? 'application' : 'devise' }
+
 module Weowe
   class Application < Rails::Application
     system("rubocop -D #{Rails.root}") if Rails.env.development?
-    # Settings in config/environments/* take precedence over those specified here.
+    # Settings in config/environments/* take precedence over those specified
+    # here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # Set Time.zone default to the specified zone and make Active Record
+    # auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names.
+    # Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # The default locale is :en and all translations from
+    # config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path
+    # += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
     config.assets.initialize_on_precompile = false
     config.i18n.enforce_available_locales = true
 
     config.to_prepare do
-        Devise::SessionsController.layout proc { |controller| user_signed_in? ? 'application' : 'devise' }
-        Devise::RegistrationsController.layout proc { |controller| user_signed_in? ? 'application' : 'devise' }
-        Devise::ConfirmationsController.layout 'application'
-        Devise::UnlocksController.layout 'application'
-        Devise::PasswordsController.layout 'application'
+      Devise::SessionsController.layout SESSIONS
+      Devise::RegistrationsController.layout REGISTRATION
+      Devise::ConfirmationsController.layout 'application'
+      Devise::UnlocksController.layout 'application'
+      Devise::PasswordsController.layout 'application'
     end
   end
 end
