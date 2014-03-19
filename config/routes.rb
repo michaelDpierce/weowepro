34fd3cc0ddab90803176d9e03f5fe8ac62604c0a
Michael Pierce <michael@affektive.com>
2014-03-18 23:01:05 -0600

    Signature pad implemented

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,7 @@ Weowe::Application.routes.draw do
   resources :charges
 
   root 'weowe_forms#dashboard'
-  get 'static_pages/help'
-  get 'static_pages/contact'
-  get 'static_pages/about'
+
   match '/dashboard', to: 'weowe_forms#dashboard', via: 'get'
 
   match '/pending', to: 'weowe_forms#pending',    via: 'get'
@@ -18,4 +16,5 @@ Weowe::Application.routes.draw do
   match '/admin', to: 'dealers#admin',    via: 'get'
 
   match '/stats', to: 'static_pages#stats',    via: 'get'
+  match '/about', to: 'static_pages#about',    via: 'get'
 end
