05f6fc8570264d03ed914bb2bf036ea26e53f1e3
Kristopher Leo Turner <KrisLeoTurner@Gmail.com>
2014-04-05 20:52:23 -0600

    another attempt to fix email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,16 +2,16 @@ Weowe::Application.configure do
   # Settings specified here will take precedence over those in
   # config/application.rb.
 
-  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'}
-  ActionMailer::Base.smtp_settings = {
-    :address        => "smtp.sendgrid.net",
-    :port           => "587",
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => ENV['SENDGRID_DOMAIN'],
-    :enable_starttls_auto => true
-  }
+  # config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'}
+  # ActionMailer::Base.smtp_settings = {
+  #   :address        => "smtp.sendgrid.net",
+  #   :port           => "587",
+  #   :authentication => :plain,
+  #   :user_name      => ENV['SENDGRID_USERNAME'],
+  #   :password       => ENV['SENDGRID_PASSWORD'],
+  #   :domain         => ENV['SENDGRID_DOMAIN'],
+  #   :enable_starttls_auto => true
+  # }
 
 
 
