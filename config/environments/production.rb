ded60f2f7eac7bc7a639596aab8b5f44e58695d1
Kristopher Leo Turner <KrisLeoTurner@Gmail.com>
2014-04-05 21:09:42 -0600

    changed another thing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Weowe::Application.configure do
   # config/application.rb.
 
   config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'}
+   config.action_mailer.delivery_method = :smtp
   # ActionMailer::Base.smtp_settings = {
   #   :address        => "smtp.sendgrid.net",
   #   :port           => "587",
