68287b76e08a8660098d53e8d53e05ca178ccc5f
Michael Pierce <michael@affektive.com>
2014-04-13 15:59:03 -0600

    Add admin column closes #144

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,8 @@ Weowe::Application.configure do
   # Settings specified here will take precedence over those in
   # config/application.rb.
 
-  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'}
-   config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'}
+  #  config.action_mailer.delivery_method = :smtp
   # ActionMailer::Base.smtp_settings = {
   #   :address        => "smtp.sendgrid.net",
   #   :port           => "587",
@@ -13,6 +13,19 @@ Weowe::Application.configure do
   #   :domain         => ENV['SENDGRID_DOMAIN'],
   #   :enable_starttls_auto => true
   # }
+
+  ActionMailer::Base.smtp_settings = {
+     :address        => 'smtp.sendgrid.net',
+     :port           => '587',
+     :authentication => :plain,
+     :user_name      => ENV['SENDGRID_USERNAME'],
+     :password       => ENV['SENDGRID_PASSWORD'],
+     :domain         => 'heroku.com'
+   }
+
+   ActionMailer::Base.delivery_method = :smtp
+
+   config.action_mailer.default_url_options = { :host => 'weowepro.herokuapp.com'
 
 
 
