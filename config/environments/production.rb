ad27ae300d5b687f81067c7d3baae005f1446b00
Michael Pierce <michael@affektive.com>
2014-04-13 14:20:53 -0600

    Attempt fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,15 +4,15 @@ Weowe::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'}
    config.action_mailer.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    :address        => "smtp.sendgrid.net",
-    :port           => "587",
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => ENV['SENDGRID_DOMAIN'],
-    :enable_starttls_auto => true
-  }
+  # ActionMailer::Base.smtp_settings = {
+  #   :address        => "smtp.sendgrid.net",
+  #   :port           => "587",
+  #   :authentication => :plain,
+  #   :user_name      => ENV['SENDGRID_USERNAME'],
+  #   :password       => ENV['SENDGRID_PASSWORD'],
+  #   :domain         => ENV['SENDGRID_DOMAIN'],
+  #   :enable_starttls_auto => true
+  # }
 
 
 
