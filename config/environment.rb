bb4837d853493ec5f7b57ec0858d609f641d1252
Kristopher Leo Turner <KrisLeoTurner@Gmail.com>
2014-04-13 18:36:29 -0600

    Fixed email on developement by pulling code from setup_sendgrid branch

--- config/environment.rb
+++ config/environment.rb
@@ -1,17 +1,19 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Initialize the Rails application.
 Weowe::Application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :address        => "smtp.sendgrid.net",
-  :port           => "587",
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
+  :user_name      => 'app22681995@heroku.com',
+  :password       => 'Wasd1313!',
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  # Use these once you figure out the variable/yml thing
+  # :user_name      => ENV['SENDGRID_USERNAME'],
+  # :password       => ENV['SENDGRID_PASSWORD'],
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,20 +2,6 @@ Weowe::Application.configure do
   # Settings specified here will take precedence over those in
   # config/application.rb.
 
-  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'}
-   config.action_mailer.delivery_method = :smtp
-  # ActionMailer::Base.smtp_settings = {
-  #   :address        => "smtp.sendgrid.net",
-  #   :port           => "587",
-  #   :authentication => :plain,
-  #   :user_name      => ENV['SENDGRID_USERNAME'],
-  #   :password       => ENV['SENDGRID_PASSWORD'],
-  #   :domain         => ENV['SENDGRID_DOMAIN'],
-  #   :enable_starttls_auto => true
-  # }
-
-
-
   config.action_mailer.delivery_method = :test
 
   # Code is not reloaded between requests.
