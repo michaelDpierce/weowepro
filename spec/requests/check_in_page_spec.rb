bebd16a40dba18f5c24f951077786f76a3b4cbdf
Kristopher Leo Turner <KrisLeoTurner@Gmail.com>
2014-04-19 16:52:23 -0600

    Updates tomaster

---
+++ spec/requests/check_in_page_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'Check In Page' do
+  let!(:dealer) { FactoryGirl.create(:dealer) }
+  let!(:user) { FactoryGirl.create(:user, dealer_id: dealer.id) }
+
+  before do
+    login_as(user, scope: :user)
+    visit users_path
+  end
+
+  subject { page }
+
+  it { should have_title('COMSA Tracker | Swimmer Check In') }
+  it { should have_selector('h1', text: 'Swimmer Check In') }
+  it { should have_content('Last Name') }
+  it { should have_content('First Name') }
+end
