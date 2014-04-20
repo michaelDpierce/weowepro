d662160c92b5853c1b1ce8b4f122cefdf8a111d5
Michael Pierce <michael@affektive.com>
2014-04-20 11:44:47 -0600

    Changed style of we owe form closes #206

--- app/models/weowe_form.rb
+++ app/models/weowe_form.rb
@@ -2,12 +2,6 @@ class WeoweForm < ActiveRecord::Base
   belongs_to :dealer
   belongs_to :user
 
-  has_many :weowe_items, dependent: :destroy
-
-  accepts_nested_attributes_for :weowe_items,
-                                reject_if: -> (weowe_item) { weowe_item[:description].blank? },
-                                allow_destroy: true
-
   validates_presence_of :customer_first_name
   validates_presence_of :customer_last_name
   validates_presence_of :stock_number
@@ -16,4 +10,6 @@ class WeoweForm < ActiveRecord::Base
   validates_presence_of :vehicle_model
   validates_presence_of :color
   validates_presence_of :sold_date
+
+  validates_length_of :description_1, :maximum => 35
 end
