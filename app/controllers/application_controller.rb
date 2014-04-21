7afadc8ddd8abf39b83dbfadf8651769bf82deb4
Michael Pierce <michael@affektive.com>
2014-04-20 19:22:56 -0600

    Fixed signup page closes #225, prevent user from navigating to new page closes #68

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:new]
   around_filter :dealer_time_zone, if: :current_user
 
   private
