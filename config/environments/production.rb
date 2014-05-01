eab4287667f8dd04bfbddaa92b0f225027e2f211
Michael Pierce <michael@affektive.com>
2014-05-01 12:06:56 -0600

    Heroku fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,10 +32,12 @@ Weowe::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(mangle: false)
+  config.assets.compress = true
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+
 
   # Generate digests for assets URLs.
   config.assets.digest = true
