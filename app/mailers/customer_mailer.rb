a54e6fa95b9411e2edccb41789b6bcafd21de735
Kristopher Leo Turner <KrisLeoTurner@Gmail.com>
2014-05-03 10:32:31 -0600

    Emails finalized
    Fixed emails, added placeholder text for new dealer form, disabled zoom
    for mobile

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -4,12 +4,12 @@ class CustomerMailer < ActionMailer::Base
   def form_approved(weowe_form)
     @weowe_form = weowe_form
     mail(to: @weowe_form.customer_email, from: "Sales@cdodge.com",
-         subject: "A copy of your due bill from #{Time.now} for your records.")
+         subject: "A copy of your due bill from #{Time.now.strftime "%d/%m/%y"} for your records.")
   end
 
   def form_completed(weowe_form)
     @weowe_form = weowe_form
     mail(to: @weowe_form.customer_email, from: "Service@cdodge.com",
-         subject: "Your vehicle is ready for pickup!. #{Time.now}")
+         subject: "Your vehicle is ready for pickup!. #{Time.now.strftime "%d/%m/%y"}")
   end
 end
