eb088beaaab4cfeacb979868ae9ad36330a04d82
Michael Pierce <michael@affektive.com>
2014-06-12 18:53:57 -0600

    Fixed dealer id

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,9 +3,9 @@ class StaticPagesController < ApplicationController
     @start_date = params[:start_date]
     @end_date = params[:end_date]
     @records = WeoweForm.where(updated_at: @start_date..@end_date)
-    @sales = WeoweForm.where(pending: true, completed: false)
-    @service = WeoweForm.where(pending: false, completed: false)
-    @completed = WeoweForm.where(pending: false, completed: true)
+    @sales = WeoweForm.where(dealer_id: current_user.dealer_id, pending: true, completed: false)
+    @service = WeoweForm.where(dealer_id: current_user.dealer_id, pending: false, completed: false)
+    @completed = WeoweForm.where(dealer_id: current_user.dealer_id, pending: false, completed: true)
   end
 
   def privacy
